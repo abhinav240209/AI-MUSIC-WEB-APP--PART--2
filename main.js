0ad659d7f5f4c62ac9f99e9f87f32c4059d5e386
abhinav240209 <83449563+abhinav240209@users.noreply.github.com>
2022-02-02 18:22:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+    canvas=createCanvas(500,400);
+    canvas.position(400,240);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video,0,0,canvas.width,canvas.height);
+}
+
+function preload() {
+    Believer=loadSound("Believer.mp3");
+    Born_for_this=loadSound("BornForThis");
+}
